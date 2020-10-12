77e679ab7a01766ca74a1f8e7450ce99f9900169
Sorli Pierre <bert.dud2011@gmail.com>
2020-10-12 11:15:36 +0200

    Création de la base des Character.

--- ProjetRPG/src/main/java/character/Hero.java
+++ ProjetRPG/src/main/java/character/Hero.java
@@ -1,5 +1,13 @@
 package character;
 
-public class Hero {
+public class Hero extends Character {
 
+	
+	public Hero() {
+		super();
+		this.atk=30;
+		this.def=20;
+		this.pv=50;
+		
+	}
 }
---
+++ ProjetRPG/src/main/java/character/Monstre.java
@@ -0,0 +1,14 @@
+package character;
+
+public class Monstre extends Character{
+	
+	
+	
+	public Monstre() {
+		super();
+		this.atk=40;
+		this.def=10;
+		this.pv=100;
+	}
+
+}
